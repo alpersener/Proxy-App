92c2bc743dc5ee540c4a4517bae91831e91ae63f
alpersener <69325145+alpersener@users.noreply.github.com>
2022-12-08 21:52:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import threading
 import time
 from tkinter import messagebox
 import requests
@@ -9,10 +9,9 @@ import concurrent.futures
 import tkinter as tk
 import customtkinter
 from urllib.request import urlopen
-import re as r
 import urllib
 import socket
-import pandas as pd
+
 
 ## INTERNET SETTINGS VE PROXY AÇIP KAPATMA
 INTERNET_SETTINGS = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
@@ -33,7 +32,7 @@ with open('proxylist.csv', 'r') as file:
 
 
 #--------------------------------------------------------------------------------------------------------------------------
-#GUI KISIMLARI
+#FONKSİYON KISIMLARI
 def proxyAc():
     button3.pack()
     button4.pack_forget()
@@ -74,15 +73,70 @@ def proxyKapat():
 
 
 def ipGoster():
-    ip = urllib.request.getproxies()
-    msg=messagebox.showinfo("IP:",ip)
+    ip_dict = urllib.request.getproxies()
+    if ip_dict:
+        ip = ip_dict['http']
+        msg = messagebox.showinfo("IP:", ip)
+    else:
+        msg = messagebox.showinfo("HATA!", "Proxy ayarları açık değil.")
 
 #def checkButtonTiklandi():
-    #for i in range(sum(1 for row in 'proxylistv2.csv')):
 
-        #time.sleep(15)  #
+       # time.sleep(15)
 
-#-------------------------------------------------------------------------------------------------------
+def get_key(name):
+    return winreg.QueryValueEx(INTERNET_SETTINGS, name)[0]
+def siradakiIp():
+    ip_list = []
+    with open("proxylistv2.csv", 'r') as f:
+        reader = csv.reader(f)
+        for satir in reader:
+            ip_list.append(satir[0])
+    sonuc = messagebox.askyesno(title="UYARI!", message="Diğer IP'ye geçmek istediğinize emin misiniz?")
+    if (sonuc == True):
+        current_ip = get_key('ProxyServer')  #geçerli ip al
+        for ip in ip_list:# IP adreslerini tek tek kontrol edecek ve geçerli IP adresini bulması için
+            if current_ip == ip:
+                current_ip_index = ip_list.index(ip)  # ip indexini bulmak için
+                break
+        if not current_ip_index:
+            current_ip_index = 0
+        next_ip_index = current_ip_index + 1  # Geçerli IP adresinin bir sonraki indeksini bul
+
+        if next_ip_index >= len(ip_list):  # Eğer bir sonraki IP adresi liste içinde yoksa ilk IP adresini kullan
+            next_ip_index = 0
+        next_ip = ip_list[next_ip_index]  # Bir sonraki IP adresini al
+        set_key('ProxyEnable', 1)
+        set_key('ProxyOverride', u'*.local;<local>')
+        set_key('ProxyServer', next_ip)
+        messagebox.showinfo("Başarılı!", message="IP adresi değiştirildi.")
+        #while True: # programı durduruyor??
+          #  time.sleep(300)
+           # siradakiIp()
+
+
+def countdown(): ##burası kullanılmıyor.
+
+    time_left = int(label1["text"]) - 1
+    label["text"] = str(time_left)
+
+
+    if time_left <= 0:
+        print("Zaman doldu!")
+        return
+
+
+    label1["text"] = str(time_left)
+
+
+    label1.after(1000, countdown)
+
+#def buttonİnvoke():
+
+    #siradakiIp()
+
+
+#---------------------------------------------------------------------------------------------------------------
 #GUI KISIMLARI
 
 
@@ -98,6 +152,9 @@ frame.pack(pady=20,padx=50,fill="both",expand=True)
 label=customtkinter.CTkLabel(master=frame,text="PROXY",font=("Roboto",44))
 label.pack(pady=12,padx=10)
 
+label1=tk.Label(master=frame)
+label1.pack()
+
 button3 = tk.Button(master=frame,text="PROXY AÇIK",bg="green")
 button4=tk.Button(master=frame,text="PROXY KAPALI",bg="red")
 
@@ -111,33 +168,15 @@ button.pack(pady=12,padx=10)
 button2=customtkinter.CTkButton(master=frame,text="Proxy kapat",command=proxyKapat)
 button2.pack(pady=12,padx=10)
 
-checkbox=customtkinter.CTkCheckBox(master=frame,text="Timer",command="checkButtonTiklandi")
+checkbox=customtkinter.CTkCheckBox(master=frame,text="Timer",command=countdown)
 checkbox.pack(pady=12,padx=10)
 
 button5=customtkinter.CTkButton(master=frame,text="Kullanılan proxy ip:",command=ipGoster)
 button5.pack(pady=12,padx=10)
 
-
-
-
+button6=customtkinter.CTkButton(master=frame,text="Sıradaki IP geç",command=siradakiIp)
+button6.pack(pady=12,padx=10)
 
 root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
