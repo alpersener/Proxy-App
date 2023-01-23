fbdf36261839fecd5c7fa4707a60bec513ece916
alpersener <69325145+alpersener@users.noreply.github.com>
2023-01-23 13:42:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import customtkinter
 from urllib.request import urlopen
 import urllib
 import socket
+from bs4 import BeautifulSoup
+from tkinter import *
 
 
 ## INTERNET SETTINGS VE PROXY AÇIP KAPATMA
@@ -33,11 +35,13 @@ with open('proxylist.csv', 'r') as file:
 
 #--------------------------------------------------------------------------------------------------------------------------
 #FONKSİYON KISIMLARI
+
 def proxyAc():
     button3.pack()
     button4.pack_forget()
     def proxyAyarlama(proxy):
         # user agent browser
+
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
         try:
@@ -65,6 +69,7 @@ def proxyAc():
     with concurrent.futures.ThreadPoolExecutor() as executor:
      executor.map(proxyAyarlama, proxylist)
 
+
 def proxyKapat():
     set_key('ProxyEnable', 0)
     button3.pack_forget()
@@ -80,10 +85,6 @@ def ipGoster():
     else:
         msg = messagebox.showinfo("HATA!", "Proxy ayarları açık değil.")
 
-#def checkButtonTiklandi():
-
-       # time.sleep(15)
-
 def get_key(name):
     return winreg.QueryValueEx(INTERNET_SETTINGS, name)[0]
 def siradakiIp():
@@ -94,46 +95,61 @@ def siradakiIp():
             ip_list.append(satir[0])
     sonuc = messagebox.askyesno(title="UYARI!", message="Diğer IP'ye geçmek istediğinize emin misiniz?")
     if (sonuc == True):
-        current_ip = get_key('ProxyServer')  #geçerli ip al
-        for ip in ip_list:# IP adreslerini tek tek kontrol edecek ve geçerli IP adresini bulması için
+        current_ip = get_key('ProxyServer')
+        for ip in ip_list:
             if current_ip == ip:
-                current_ip_index = ip_list.index(ip)  # ip indexini bulmak için
+                current_ip_index = ip_list.index(ip)
                 break
         if not current_ip_index:
             current_ip_index = 0
-        next_ip_index = current_ip_index + 1  # Geçerli IP adresinin bir sonraki indeksini bul
+        next_ip_index = current_ip_index + 1
 
-        if next_ip_index >= len(ip_list):  # Eğer bir sonraki IP adresi liste içinde yoksa ilk IP adresini kullan
+        if next_ip_index >= len(ip_list):
             next_ip_index = 0
-        next_ip = ip_list[next_ip_index]  # Bir sonraki IP adresini al
+        next_ip = ip_list[next_ip_index]
         set_key('ProxyEnable', 1)
         set_key('ProxyOverride', u'*.local;<local>')
         set_key('ProxyServer', next_ip)
         messagebox.showinfo("Başarılı!", message="IP adresi değiştirildi.")
-        #while True: # programı durduruyor??
-          #  time.sleep(300)
-           # siradakiIp()
 
 
-def countdown(): ##burası kullanılmıyor.
-
-    time_left = int(label1["text"]) - 1
-    label["text"] = str(time_left)
 
 
-    if time_left <= 0:
-        print("Zaman doldu!")
-        return
 
 
-    label1["text"] = str(time_left)
+def ac():
+    yeni_pencere = tk.Toplevel(root)
+    yeni_pencere.title("Yeni pencere")
+    yeni_pencere.geometry("400x500")
+    def veriCek():
+        url = entry.get()
+        sayfa_icerigi = requests.get(url)
+        soup = BeautifulSoup(sayfa_icerigi.content, "html.parser")
+        sonuclar = soup.find_all('div', class_='card-body')
+        for sonuc in sonuclar:
+            liste.insert(tk.END, sonuc.text)
+    liste = tk.Listbox(yeni_pencere)
+    liste.pack(fill="both")
+    entry=tk.Entry(yeni_pencere)
+    entry.pack()
+    dugme=tk.Button(yeni_pencere, text='Veri Çek', command=veriCek)
+    dugme.pack()
 
 
-    label1.after(1000, countdown)
+def getContent():
+  global veri_cekici_pencere
+  veri_cekici_pencere = customtkinter.CTkToplevel()
+  veri_cekici_pencere.title('Veri Çekici')
+  global url_giris
+  global listeleme
 
-#def buttonİnvoke():
+def zamanlamaIp():
+    while True:
+        time.sleep(300)
+        siradakiIp()
 
-    #siradakiIp()
+thread = threading.Thread(target=zamanlamaIp)
+thread.start()
 
 
 #---------------------------------------------------------------------------------------------------------------
@@ -145,6 +161,7 @@ customtkinter.set_default_color_theme("dark-blue")
 
 root=customtkinter.CTk()
 root.geometry("500x350")
+checkbox_var=tk.IntVar()
 
 frame=customtkinter.CTkFrame(master=root)
 frame.pack(pady=20,padx=50,fill="both",expand=True)
@@ -152,12 +169,10 @@ frame.pack(pady=20,padx=50,fill="both",expand=True)
 label=customtkinter.CTkLabel(master=frame,text="PROXY",font=("Roboto",44))
 label.pack(pady=12,padx=10)
 
-label1=tk.Label(master=frame)
-label1.pack()
+
 
 button3 = tk.Button(master=frame,text="PROXY AÇIK",bg="green")
 button4=tk.Button(master=frame,text="PROXY KAPALI",bg="red")
-
 
 button=customtkinter.CTkButton(master=frame,text="Proxy Aç",command=proxyAc)
 button.pack(pady=12,padx=10)
@@ -165,14 +180,22 @@ button.pack(pady=12,padx=10)
 button2=customtkinter.CTkButton(master=frame,text="Proxy kapat",command=proxyKapat)
 button2.pack(pady=12,padx=10)
 
-checkbox=customtkinter.CTkCheckBox(master=frame,text="Timer",command=countdown)
-checkbox.pack(pady=12,padx=10)
+#checkbox=customtkinter.CTkCheckBox(master=frame,text="Timer",command="")
+#checkbox.pack(pady=12,padx=10)
 
 button5=customtkinter.CTkButton(master=frame,text="Kullanılan proxy ip:",command=ipGoster)
 button5.pack(pady=12,padx=10)
 
 button6=customtkinter.CTkButton(master=frame,text="Sıradaki IP geç",command=siradakiIp)
 button6.pack(pady=12,padx=10)
+
+button7=customtkinter.CTkButton(master=frame,text="getContent",command=ac)
+button7.pack(pady=12,padx=10)
+
+
+
+
+
 
 root.mainloop()
 
